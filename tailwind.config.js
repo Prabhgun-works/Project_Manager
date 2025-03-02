17d2a6875ae47d1fc149ec61222600dfb2ba8b64
Prabhgun-works <prabhgun.web@gmail.com>
2025-03-02 16:17:55 +0530

    Final Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
-const { patterns } = require('tailwindcss-patterns');
-
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ], 
   theme: {
-    extend: {
-      patterns: {
-        opacities: { default: '0.1' },
-        sizes: { default: '20px' },
-      },
-    },
+    extend: {},
   },
-  plugins: [patterns],
+  plugins: [],
 };
